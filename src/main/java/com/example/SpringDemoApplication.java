f0b00eb5af3653134468f7271a86d8788db51b24
jy.kim2 <jy.kim2@piolink.com>
2023-08-17 09:22:57 +0900

    hello world

--- src/main/java/com/example/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemoApplication.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringDemoApplication {
 
@@ -10,4 +13,10 @@ public class SpringDemoApplication {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
+	@GetMapping(value = "/")
+	public String HelloWorld(){
+		return "Hello World";
+	}
 }
+
+
